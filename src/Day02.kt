efc2810e2ef487d0e0716e9a4965b1ff2e9a118e
Daniel Kovari <74988054+captnketchup@users.noreply.github.com>
2024-12-02 20:51:49 +0100

    feat: add part2 solution for day2

--- src/Day02.kt
+++ src/Day02.kt
@@ -9,14 +9,28 @@ fun main() {
 
         val inputCopy = input.toMutableList()
         var iteratorElement = inputCopy.removeFirst()
-        inputCopy.forEach{
-            if (  kotlin.math.abs(it - iteratorElement) !in 1..3) {
+        inputCopy.forEach {
+            if (kotlin.math.abs(it - iteratorElement) !in 1..3) {
                 return false
             }
             iteratorElement = it
         }
 
         return true
+    }
+
+    fun IsSafeWithError(input: List<Int>): Boolean {
+        if (IsSafe(input)) {
+            return true
+        }
+
+        input.forEachIndexed { index, element ->
+            val mutableInput = input.toMutableList().apply { removeAt(index) }
+            if (IsSafe(mutableInput)) {
+                return true
+            }
+        }
+        return false
     }
 
     fun part1(input: List<String>): Int {
@@ -32,9 +46,20 @@ fun main() {
 
     }
 
+    fun part2(input: List<String>): Int {
+        return input
+            .map { it.split(" ") }
+            .map {
+                it.map { element ->
+                    element.toInt()
+                }
+            }
+            .map { IsSafeWithError(it) }
+            .count { it }
+    }
+
     val input = readInput("Day02")
 
-    println(part1(input))
-
-//    fun part1(input: List<Int>): Int {}
+    println("part1: ${part1(input)}")
+    println("part2: ${part2(input)}")
 }
